6874ee6c036e4ecac6902efe7f0b069c3aa98a09
Samuel Shen <sshen05@terpmail.umd.edu>
2022-05-31 22:27:59 -0400

    Add capability to populate and clear display

--- script.js
+++ script.js
@@ -35,3 +35,39 @@ function operate(num1, num2, operator) {
             return 'Error';
     }
 }
+
+/**
+ * Put text onto the calculator display
+ * @param {String} text The text to display
+ */
+function displayText(text) {
+    const display = document.querySelector('.display');
+    display.textContent = text;
+}
+
+/**
+ * Event handler to update the display when the user clicked a number button
+ */
+function registerNumberClick() {
+    const display = document.querySelector('.display');
+    let num = display.textContent;
+    num += this.textContent;
+    displayText(num);
+}
+
+function clearDisplay() {
+    const display = document.querySelector('.display');
+    display.textContent = '';
+}
+
+function main() {
+    const numberButtons = document.querySelectorAll('.number-btn');
+    for(const btn of numberButtons) {
+        btn.addEventListener('click', registerNumberClick);
+    }
+
+    const clearButton = document.querySelector('#clear');
+    clearButton.addEventListener('click', clearDisplay);
+}
+
+main();
